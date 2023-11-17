4d43e8b4b18ecdee804fa873ef59fa0037b0b6d3
zz <xzrunner@gmail.com>
2023-11-18 06:45:08 +0800

    add code block tree

--- include/brepvm/Compiler.h
+++ include/brepvm/Compiler.h
@@ -3,6 +3,7 @@
 #include <easyvm/VM.h>
 
 #include <array>
+#include <stack>
 
 namespace brepvm
 {
@@ -23,13 +24,34 @@ public:
 		return m_precomp_cond_branch; 
 	}
 
+	void PushBlock(const std::string& name, size_t pos);
+	void PopBlock(size_t pos);
+
+	void PrintBlockTree();
+
 private:
 	struct Register
 	{
 		bool used = false;
 		bool keep = false;
 	};
+
+	struct Block
+	{
+		std::string name;
+		size_t begin, end;
+
+		std::vector<std::shared_ptr<Block>> children;
+	};
+
+private:
+	void PrintBlock(const std::shared_ptr<Block>& b, int level);
+
+private:
 	std::array<Register, REGISTER_COUNT> m_registers;
+
+	std::shared_ptr<Block> m_root_block = nullptr;
+	std::stack<std::shared_ptr<Block>> m_block_path;
 
 	bool m_precomp_cond_branch = false;
 
--- source/Compiler.cpp
+++ source/Compiler.cpp
@@ -52,4 +52,77 @@ void Compiler::ExpectRegFree()
     }
 }
 
+void Compiler::PushBlock(const std::string& name, size_t pos)
+{
+    auto block = std::make_shared<Block>();
+    block->name = name;
+    block->begin = pos;
+
+    if (m_block_path.empty())
+    {
+        assert(!m_root_block);
+        m_root_block = block;
+    }
+    else
+    {
+        auto parent = m_block_path.top();
+        parent->children.push_back(block);
+    }
+
+    m_block_path.push(block);
+}
+
+void Compiler::PopBlock(size_t pos)
+{
+    assert(!m_block_path.empty());
+    auto block = m_block_path.top();
+    block->end = pos;
+
+    m_block_path.pop();
+}
+
+void Compiler::PrintBlockTree()
+{
+    PrintBlock(m_root_block, 0);
+}
+
+void Compiler::PrintBlock(const std::shared_ptr<Block>& b, int level)
+{
+    if (!b || b->begin == b->end) {
+        return;
+    }
+
+    //if (b->end - b->begin < 16) {
+    //    return;
+    //}
+
+    if (b->children.size() == 1)
+    {
+        auto& c = b->children.front();
+        if (b->begin == c->begin && b->end == c->end) 
+        {
+            PrintBlock(c, level);
+            return;
+        }
+    }
+
+    auto print_header = [&level]() 
+    {
+        for (int i = 0; i < level; ++i) {
+            //printf("\t");
+            printf(" ");
+        }
+    };
+
+    print_header();
+    printf("%s begin %d\n", b->name.c_str(), b->begin);
+
+    for (auto& child : b->children) {
+        PrintBlock(child, level + 1);
+    }
+
+    print_header();
+    printf("%s end %d\n", b->name.c_str(), b->end);
+}
+
 }
